5fce66ace0a8a1f27963ddb95aafc8d31fbc7997
Pratik1618 <kadampratik18@gmail.com>
2025-04-25 17:56:35 +0530

    middleware

--- authMiddleware/adminAuthMiddleware.js
+++ authMiddleware/adminAuthMiddleware.js
@@ -2,24 +2,24 @@ const jwt = require('jsonwebtoken');
 
 const adminAuthMiddleware = (req, res, next) => {
 
-    const authHeader  = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json({ message: 'No token provided' });
     }
 
     const token = authHeader.split(' ')[1];
-    try{
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
 
-        if(decoded.role !== 'admin') {
+        if (decoded.role !== 'admin') {
             return res.status(403).json({ message: 'Access denied' });
         }
- next();
+        next();
 
 
-    }catch(error){
+    } catch (error) {
         return res.status(401).json({ message: 'Invalid token' });
     }
 };
