c7f1177c635e8310ff2905b1539149fed7904cde
Pratik1618 <kadampratik18@gmail.com>
2025-05-03 16:07:39 +0530

    commmit

--- routes/scheduleRoute.js
+++ routes/scheduleRoute.js
@@ -2,14 +2,15 @@ const express = require('express');
 const Schedule = require('../model/schedule');
 const ticketGenerator = require('../utils/ticketGenerator');
 const router = express.Router();
+const adminAuthMiddleware = require('../authMiddleware/adminAuthMiddleware')
 
-router.post('/createSchedule',async(req,res)=>{
-    try{
-        const {  storeId, technicianId, scheduleType, scheduleDate } = req.body;
-        if (  !storeId || !technicianId || !scheduleType || !scheduleDate) {
+router.post('/createSchedule', adminAuthMiddleware, async (req, res) => {
+    try {
+        const { storeId, technicianId, scheduleType, scheduleDate } = req.body;
+        if (!storeId || !technicianId || !scheduleType || !scheduleDate) {
             return res.status(400).json({ message: 'Please fill all fields' });
         }
-       const ticketNo = ticketGenerator();
+        const ticketNo = ticketGenerator();
         const newSchedule = new Schedule({
             ticketNo,
             storeId,
@@ -20,19 +21,19 @@ router.post('/createSchedule',async(req,res)=>{
         await newSchedule.save();
         res.status(201).json({
             message: 'Schedule created successfully',
-           data:newSchedule
+            data: newSchedule
         });
-    }catch(error){
+    } catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Server error' });
     }
 })
 
-router.get('/schedules',async(req,res)=>{
-    try{
-        const schedules = await Schedule.find().populate({path:'storeId',populate:{path:'clientId',select:'clientName'}}).populate('technicianId').sort({scheduleDate:-1});
+router.get('/schedules', async (req, res) => {
+    try {
+        const schedules = await Schedule.find().populate({ path: 'storeId', populate: { path: 'clientId', select: 'clientName' } }).populate('technicianId').sort({ scheduleDate: -1 });
         res.status(200).json(schedules);
-    }catch(error){
+    } catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Server error' });
     }
